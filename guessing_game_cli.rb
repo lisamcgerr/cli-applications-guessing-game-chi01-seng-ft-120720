0716305c8caf96546cd650c0428035f9a769e04f
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-12-07 05:18:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   random_number = rand(1..6)
   puts "Guess a number between 1 and 6!"
   user_input = gets.chomp
-    if user_input == "exit"
+    if user_input.downcase == "exit"
       puts "Goodbye!"
     elsif user_input.to_i == random_number
       puts "You guessed the correct number!"
